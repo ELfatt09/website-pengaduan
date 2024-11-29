c1345e4b574d59817b51fae448012d4e83650074
Sevalino Elfata <sevalinoelfata8@gmail.com>
2024-11-29 22:28:16 +0700

    "Updated login and register scripts to use password hashing and verification"

--- auth/register.php
+++ auth/register.php
@@ -8,7 +8,7 @@ if (isset($_POST['register'])) {
         exit();
     }
     $username = $_POST['username'];
-    $password = $_POST['password'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     // Validation
     $errors = [];
